896dd6b541937f94bf6fc28290cb85b42185e9f9
Secher <ekko-01@foxmail.com>
2025-10-02 04:05:27 +0800

    change header:SECHER NBIW

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,26 +1,38 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "kanagawa",
+  theme = "kanagawa",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 -- Configure LSP hover for better markdown display
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
+M.nvdash = { load_on_startup = true,
+  header = {
+  "                                                                                           ",
+  "   ███████╗███████╗ ██████╗██╗  ██╗███████╗██████╗     ███╗   ██╗██████╗ ██╗██╗    ██╗  ",
+  "   ██╔════╝██╔════╝██╔════╝██║  ██║██╔════╝██╔══██╗    ████╗  ██║██╔══██╗██║██║    ██║  ",
+  "   ███████╗█████╗  ██║     ███████║█████╗  ██████╔╝    ██╔██╗ ██║██████╔╝██║██║ █╗ ██║  ",
+  "   ╚════██║██╔══╝  ██║     ██╔══██║██╔══╝  ██╔══██╗    ██║╚██╗██║██╔══██╗██║██║███╗██║  ",
+  "   ███████║███████╗╚██████╗██║  ██║███████╗██║  ██║    ██║ ╚████║██████╔╝██║╚███╔███╔╝  ",
+  "   ╚══════╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝    ╚═╝  ╚═══╝╚═════╝ ╚═╝ ╚══╝╚══╝   ",
+  "                                                                                           ",
+}
+}
+M.ui = {
+  statusline = {
+    theme = "default",
+    separator_style = "round",
+  },
+}
 
 return M
