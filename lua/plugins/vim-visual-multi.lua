d34f19db694538fc494e438d8a2d592c1b0ea3ba
Secher <ekko-01@foxmail.com>
2025-10-02 16:01:31 +0800

    添加vim-visual-multi插件

---
+++ lua/plugins/vim-visual-multi.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "mg979/vim-visual-multi",
+    branch = "master",
+    event = "VeryLazy",
+    init = function()
+      vim.g.VM_maps = {
+        ["Find Under"] = "<C-d>",           -- Ctrl+d 选择下一个相同单词
+        ["Find Subword Under"] = "<C-d>",
+        ["Select All"] = "<C-S-l>",         -- Ctrl+Shift+l 选择所有
+        ["Skip Region"] = "<C-x>",          -- Ctrl+x 跳过当前
+        ["Remove Region"] = "<C-p>",        -- Ctrl+p 取消选择
+        ["Add Cursor Down"] = "<C-Down>",   -- Ctrl+Down 向下添加光标
+        ["Add Cursor Up"] = "<C-Up>",       -- Ctrl+Up 向上添加光标
+      }
+    end,
+  },
+}
