1a26d74ae6a9aebfa7e14660c977986657521296
Anivia-ad <1961055061@qq.com>
2025-10-06 17:43:26 +0800

    优化autocmd.lua配置

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -4,27 +4,34 @@ require "nvchad.autocmds"
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- 创建自动命令组
-local numbertoggle = vim.api.nvim_create_augroup("numbertoggle", { clear = true })
+local api = vim.api
+local numbertoggle = api.nvim_create_augroup("numbertoggle", { clear = true })
 
 -- 在插入模式下显示绝对行号，其他模式显示相对行号
-vim.api.nvim_create_autocmd(
+api.nvim_create_autocmd(
   { "BufEnter", "FocusGained", "InsertLeave" },
   { group = numbertoggle, command = "set relativenumber" }
 )
 
-vim.api.nvim_create_autocmd(
+api.nvim_create_autocmd(
   { "BufLeave", "FocusLost", "InsertEnter" },
   { group = numbertoggle, command = "set norelativenumber" }
 )
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = '复制后高亮显示',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+-- 自定义高亮组颜色
+api.nvim_set_hl(0, "YankHighlight", {
+  bg = "#c8c094",
+  fg = "#1f1f28", -- 前景色：深色文字（可选，确保文字可读）
+})
+
+-- 复制后高亮显示
+api.nvim_create_autocmd("TextYankPost", {
+  desc = "复制后高亮显示",
+  group = api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
-    vim.highlight.on_yank({
-      higroup = 'IncSearch',  -- 使用的高亮组
-      timeout = 400,           -- 高亮持续时间(毫秒)
-    })
+    vim.highlight.on_yank {
+      higroup = "YankHighlight", -- 使用自定义高亮组
+      timeout = 200, -- 高亮持续时间(毫秒)
+    }
   end,
 })
